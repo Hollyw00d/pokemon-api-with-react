bdcd28f24d79c02845679f08fb300fcfae1f8bb5
Matt Jennings <matt@mattjennings.net>
2026-05-12 08:54:15 -0700

    simplied main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import SearchPokemon from './SearchPokemon';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <SearchPokemon />
-  </React.StrictMode>
+  </StrictMode>
 );
