46f6a842583dfce821489b2a9435655f6db20825
Matt Jennings <matt@mattjennings.net>
2026-05-11 18:37:56 -0700

    simplied GetPokemon component

--- src/GetPokemon.jsx
+++ src/GetPokemon.jsx
@@ -2,13 +2,12 @@ import FailGetPokemon from './FailGetPokemon';
 import SuccessGetPokemon from './SuccessGetPokemon';
 
 export default function GetPokemon({ query, error }) {
-  if (error) {
-    return <FailGetPokemon error={error} />;
-  }
-
-  if (query) {
-    return <SuccessGetPokemon query={query} />;
-  }
-
-  return null;
+  return (
+    <>
+      <div role="alert" aria-atomic="true">
+        {error && <FailGetPokemon error={error} />}
+      </div>
+      {query && <SuccessGetPokemon query={query} />}
+    </>
+  );
 }
